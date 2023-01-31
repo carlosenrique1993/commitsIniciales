09d871579bbf20408cce4179e83bc6ecdf141cc5
carlosenrique1993 <carlos.elias@jaliscoedu.mx>
2023-01-31 09:19:22 -0600

    commits iniciales2

---
+++ commitsIniciales.js
@@ -0,0 +1,8 @@
+let nombre1 = 'juan';
+let nombre2 = 'juanito';
+
+if (nombre1.length != nombre2.length) {
+    console.log('no son igual de largas');
+} else {
+    console.log('son igual de largas');
+}
